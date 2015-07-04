ec7578d3d020aca8a3c00e04bc774b08ffc58741
erbn <r-ehl@hotmail.de>
2015-07-04 16:39:37 +0200

    Initial

---
+++ src/threading/Principal.java
@@ -0,0 +1,40 @@
+package threading;
+
+import java.awt.Dimension;
+import java.awt.Graphics;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.SwingUtilities;
+
+
+public class Principal {
+
+	static String title = "Jail";
+	
+	public static void main(String[] args) {
+	      javax.swing.SwingUtilities.invokeLater(new Runnable() {
+	          public void run() {
+	             guiCreation();
+	          }
+	       });
+	}
+	public static void guiCreation() {
+		SwingUtilities.isEventDispatchThread();
+		JFrame f = new JFrame(title);
+		f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		f.add(new Ball());
+		f.pack();
+		f.setVisible(true);
+	}
+}
+
+class Ball extends JPanel {
+	public Dimension getPreferredSize(){
+		return new Dimension(700,300);
+	}
+	public void paintComponent(Graphics g) {
+		super.paintComponent(g);
+		g.drawString("Example", 10, 20);
+	}
+}
